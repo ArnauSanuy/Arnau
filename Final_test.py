2a211bbce95abee9f22cf8b2f3247c805b843d72
ArnauSanuy <104277029+ArnauSanuy@users.noreply.github.com>
2025-01-13 11:06:53 +0100

    Update Final_test.py

--- Final_test.py
+++ Final_test.py
@@ -113,46 +113,23 @@ for _m_dot in m_dot:
 T_final_values = T_eq_values * u.K
 
 m_proof = 5e7
-P6 = np.linspace(1.5, 3, 50)
-T7 = np.linspace(0.2, 4, 50)
-
-new_m_dot = (
-    4.16e-5
-    *(10.6/5.17)
-    *np.power(32.2, 1/2)
-    *np.power(1/0.3, 1/3)
-    *np.power(4.3e-5, 1/6)
-    *np.power(0.5, 8/3)
-    *P6
-    *np.power(T7, -5/2))
-
-new_x_m = x_m_appendix_B(new_m_dot)
-new_alpha_c = alpha_c(theta_from_T(T_final_values), new_m_dot)
-mu_p = vp(new_x_m, new_m_dot, m_proof, theta_from_T(T_eq_values * u.K)).value
+_new_m_dot = new_m_dot(P6, T7)
+new_x_m = x_m_appendix_B(_new_m_dot)
+new_alpha_c = alpha_c(theta_from_T(T_final_values), _new_m_dot)
+mu_p = vp(new_x_m, _new_m_dot, m_proof, theta_from_T(T_eq_values * u.K)).value
 mean = mean(mu_p)
 
 v1 = np.logspace(np.log10(mean), 8, 50)
 v2 = np.logspace(np.log10(mean),  21, 50)
 v = np.logspace(8, 21, 50)
 
-first_part = v1*L_synch(m_proof, new_m_dot, new_x_m, T_eq_values, v1)
-second_part = v2*L_compton(new_alpha_c, m_proof, new_m_dot, new_x_m, T_eq_values, v2, mean)
-third_part = v2*L_bremms(T_eq_values, m_proof, new_m_dot, v2)
-flux_2 = second_part + third_part
-united_list = [item for sublist in [flux_2, first_part] for item in sublist]
-v_list = [item for sublist in [v2, v1] for item in sublist]
-
-first_flux = Total_flux(m_proof, new_m_dot, new_x_m, T_eq_values, new_alpha_c, v, v1, v2, mu_p)
-
-calculus = np.log10(first_flux.value)
+first_flux = Total_flux(m_proof, _new_m_dot, new_x_m, T_eq_values, new_alpha_c, v, v1, v2, mean)
+calculus = np.log10(first_flux)
 
 fig, ax1 = plt.subplots()
 
 ax1.plot(ref_m_dot, ref_T_e, ls="--", label="reference")
 ax1.plot(np.log10(m_dot), np.asarray(T_eq_values) / 1e9, label="implementation")
-# ax2 = ax1.twinx()
-# ax2.semilogy(ref_m_dot_x_m, ref_x_m, ls="--", label="reference")
-# ax2.semilogy(np.log10(m_dot), np.asarray(x_m_values_1), label="implementation")
 ax1.legend()
 ax1.set_ylim([0.8, 5.6])
 ax1.set_xlim([-8, 0])
@@ -160,11 +137,9 @@ ax1.set_xlabel(r"$Log(\dot{m})$")
 ax1.set_ylabel(r"$T_e\,/\,(10^{9}\,{\rm K})$")
 plt.show()
 
-#plt.plot(np.log10(v_list), np.log10(united_list))
-plt.plot(np.log10(v1), np.log10(first_part))
-#plt.plot(np.log10(v2), np.log10(second_part))
-#plt.plot(np.log10(v2), np.log10(third_part))
-plt.plot(np.log10(v2), np.log10(second_part + third_part))
+
+plt.plot(np.log10(v1), np.log10(first_flux[0]), color = 'b') 
+plt.plot(np.log10(v2), np.log10(first_flux[1]), color = 'b')
 plt.show()
 
 
@@ -191,9 +166,9 @@ ax1.set_xlim([0, -8])
 plt.show()
 
 """
-
 theta_e_final = theta_from_T(T_final_values)
 alpha_first_mass = one_minus_alpha_c(theta_e_final, _x_m, m, m_dot)
+print(alpha_first_mass)
 
 
 # Test 3: try to reproduce Fig. 2 and Fig. 3 for the second mass
