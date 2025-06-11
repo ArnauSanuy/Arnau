b1b6678c700c52fe0ea8fa96bdcd003f0c445480
ArnauSanuy <104277029+ArnauSanuy@users.noreply.github.com>
2025-06-11 11:58:19 +0200

    Initial_flux_compacted.py

--- Initial_flux_compacted.py
+++ Initial_flux_compacted.py
@@ -1,15 +1,10 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Jan 28 14:10:41 2025
-
-@author: HP
-"""
-
 import numpy as np
 import astropy.units as u
 from radiation import *
 import matplotlib.pyplot as plt
 from statistics import mean, pvariance, pstdev
+from astropy.table import Table, Column, join
+import pandas as pd
 
 # Now, we are going to present the different plots of Fig. 1 from Rohan Mahadevan
 # paper of 1997. The structure of x_m is based on his approximation formula.
@@ -55,19 +50,39 @@ m_dot_4 = 24e-4
 # # Now,what we do is to present the four implementations in just on plot.
 
 
-fig, ax = plt.subplots(figsize=(8, 6))
+fig, ax = plt.subplots(figsize=(10, 8))
 Figure_initial_flux(m, m_dot_1, ref_m_dot_1, ref_T_e_1, ax=ax, color = 'blue')
 Figure_initial_flux(m, m_dot_2, ref_m_dot_2, ref_T_e_2, ax=ax, color = 'purple')
 Figure_initial_flux(m, m_dot_3, ref_m_dot_3, ref_T_e_3, ax=ax, color = 'green')
 Figure_initial_flux(m, m_dot_4, ref_m_dot_4, ref_T_e_4, ax=ax, color = 'black')
-#ax.legend(['m_dot=3e-4', 'm_dot=6e-4', 'm_dot=12e-4', 'm_dot=24e-4'])
+ax.legend()
+plt.savefig("Initial_flux_four_cases.png", dpi=300, bbox_inches='tight')
 plt.show()
 
+
+
+cigale_file = f'NGC4261_table'
+cigale = Table.read(f'{cigale_file}')
+cigale=cigale.to_pandas()
+df = pd.DataFrame(cigale)
+t = Table.from_pandas(df)
+
+Luminosity = 4*np.pi*df['col3']*np.power(35e6*(3.0856e18),2)
 
 fig, ax = plt.subplots(figsize=(8, 6))
-Figure_initial_flux_woref(m, m_dot_1, ax=ax, color = 'blue')
-Figure_initial_flux_woref(m, m_dot_2, ax=ax, color = 'purple')
-Figure_initial_flux_woref(m, m_dot_3, ax=ax, color = 'green')
-Figure_initial_flux_woref(m, m_dot_4, ax=ax, color = 'red')
-ax.legend(['m_dot=3e-4', 'm_dot=6e-4', 'm_dot=12e-4', 'm_dot=24e-4'])
+Figure_initial_flux_woref(m, m_dot_1, ax=ax, color = 'blue', label = 'm_dot=3e-4')
+Figure_initial_flux_woref(m, m_dot_2, ax=ax, color = 'purple', label='m_dot=6e-4')
+Figure_initial_flux_woref(m, m_dot_3, ax=ax, color = 'green', label='m_dot=12e-4')
+Figure_initial_flux_woref(m, m_dot_4, ax=ax, color = 'red', label='m_dot=24e-4')
+ax.plot(np.log10(df['col1']), np.log10(Luminosity), 'o', markersize=1)
+ax.legend()
 plt.show()
+
+
+
+
+
+# plt.figure(figsize=(8,6))
+# plt.plot(np.log10(df['col1']), np.log10(df['col3']), 'o', markersize=1)
+# plt.xlim(8,21)
+# plt.show()
